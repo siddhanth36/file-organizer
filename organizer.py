b2ac580857730a26b34121873fbdc5e34f0cd526
siddhanth36 <sksiddhanth69@gmail.com>
2025-08-10 20:32:53 +0530

    organizer.py

--- organizer.py
+++ organizer.py
@@ -13,7 +13,7 @@ DIRECTORIES = {
 }
 
 # Path to organize (change this to your messy folder!)
-target_dir = "home/username/Downloads"  # 🚨 Update this path!
+target_dir = "/path/to/your/messy/folder"  # 🚨 Update this path!
 
 def create_folders():
     """Create folders if they don't exist."""
